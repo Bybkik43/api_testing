5552cab5d60142548775417df1b4f37741671fbb
Саша <byblik@inbox.ru>
2023-04-09 20:22:48 +0300

    Initial commit

---
+++ api/questions_api.py
@@ -0,0 +1,63 @@
+import self as self
+
+from api.client import Client
+import json
+
+
+class Api(Client):
+    USERS = '//users'
+    BASE_URL = 'https:/reqres.in/api'
+
+    def list_users(self):
+        url = self.BASE_URL + self.USERS + "?page=2"
+        return self.get(url)
+api = Api()
+
+    def single_user_not_found(self):
+        url = self.BASE_URL + self.USERS + '/23'
+        return self.get(url)
+
+    def single_user(self):
+        url = self.BASE_URL + self.USERS + '/2'
+        return self.get(url)
+
+    def delete_user(self, id: int):
+        url = self.BASE_URL + self.USERS + F"{id}"
+        return self.delete(url)
+
+    def create (self, name: str, job: str):
+        url = self.BASE_URL + self.USERS
+        payload = json.dumps({
+            "name": F"{name}",
+            "job": F"{job}"
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        return self.post(url, headers, payload)
+
+
+    def registration(self, email: str, password: str):
+        url = self.BASE_URL + self.USERS
+        payload = json.dumps({
+            "email": F"{eve.holt@reqres.in}",
+            "password": F"{ }"
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        return self.post(url, headers, payload)
+
+
+    def registration_inc(self, email: str):
+        url = self.BASE_URL + self.USERS
+        payload = json.dumps({
+            "email": F"{eve.holt @ reqres. in}",
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        return self.post(url, headers, payload)
+
+
---
+++ tests/test_registration.py
@@ -0,0 +1,35 @@
+import requests
+from http import HTTPStatus
+import api.questions_api
+
+
+def registration():
+    url = 'https://reqres.in/api/register'
+    email = 'eve.holt@reqres.in'
+    password = '  '
+    res = api.questions_api.registration(email, password)
+
+    assert res.status_code == HTTPStatus.CREATED
+    assert res.json()['email'] == email
+    assert res.json()['password'] == password
+
+    assert api.delete_user(res.json()(['id']).status_code == HTTPStatus.NO_CONTENT
+
+
+    response = requests.get(url)
+    assert response.status_code == HTTPStatus.OK  # 200
+
+def registration_inc():
+    url = 'https://reqres.in/api/register'
+    email = 'eve.holt@reqres.in'
+
+    res = api.questions_api.registration(password)
+
+        assert res.status_code == HTTPStatus.CREATED
+        assert res.json()['email'] == email
+
+        assert api.delete_user(res.json()(['id']).status_code == HTTPStatus.NO_CONTENT
+
+        response = requests.get(url)
+        assert response.status_code == 400
+print ('error:Missing password")
